ea769ed9579917d7406bf384b98f3e68b09dd1ee
vinayak9130009091 <vinayakkumbhar@hotmail.com>
2024-10-26 18:26:38 +0530

    25-10

---
+++ contact-backend/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const dbconnect = require('./database/dbconnect');
+const app = express();
+const cors = require('cors');
+const contactRoutes = require('./routers/contactRoutes');
+const AccountsRoutes = require('./routers/AccountsRoutes');
+const AssignTags = require('./middleware/assignbulktags')
+const mongoose = require('mongoose');
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+//   Routes for contacts
+app.use('/contacts', contactRoutes)
+
+app.use('/accounts', AccountsRoutes)
+
+app.use('/', AssignTags)
+
+//! assignbulktags Routes
+const assignbulktags = require("./middleware/assignbulktags");
+app.use("/assignbulktags", assignbulktags);
+
+
+//! sendBulkEmails Routes
+const sendBulkEmails = require("./middleware/sendBulkEmails");
+app.use("/", sendBulkEmails);
+
+
+//Cors Polycy 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true'); // Set the header to 'true' to allow credentials
+    next();
+  });
+
+// database connect
+dbconnect()
+
+
+const port = process.env.PORT || 7000;
+
+app.listen(port, ()=>{
+    console.log(`connection is live at port no. ${port}`);
+})
